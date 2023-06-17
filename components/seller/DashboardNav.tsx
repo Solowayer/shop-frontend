74c9b5b2b7d9d8dcabca013fed1c2bd7137db89c
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-06-17 21:39:24 +0300

    DashboardNav change

--- components/seller/DashboardNav.tsx
+++ components/seller/DashboardNav.tsx
@@ -1,11 +1,35 @@
+'use client'
+
 import React from 'react'
-import StyledLink from '../ui/StyledLink'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function DashboardNav() {
+	type NavItemsType = {
+		href: string
+		title: string
+	}
+
+	const navItems: NavItemsType[] = [
+		{ href: 'seller/dashboard', title: 'Дашборд' },
+		{ href: 'seller/dashboard/products', title: 'Всі товари' }
+	]
+
+	const pathname = usePathname()
+
 	return (
-		<div className="w-[280px] flex flex-col px-6 py-6 border-r h-full">
-			<StyledLink href="seller/dashboard">Дашборд</StyledLink>
-			<StyledLink href="seller/dashboard/products">Всі товари</StyledLink>
+		<div className="w-[280px] flex flex-col px-6 py-6 border-r h-full gap-1">
+			{navItems.map((item, index) => (
+				<Link key={index} href={item.href}>
+					<div
+						className={`py-2 px-4 rounded w-full hover:bg-zinc-200 ${
+							pathname === `/${item.href}` ? 'bg-zinc-200 font-medium' : ''
+						}`}
+					>
+						{item.title}
+					</div>
+				</Link>
+			))}
 		</div>
 	)
 }
