0c56327a3787ba4a751f4bff31a2eb0f901f0c29
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-07-12 17:05:38 +0300

    Revert "Pagination Store, pagination pages etc"
    This reverts commit 7a85f887c25e7ea95156bdc93ac3aee052604f1a.

---
+++ app/(main)/all-products/page.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import Products from '@/components/products'
+import ProductService from '@/services/product.service'
+import { Button, Spinner } from '@/components/ui'
+import { ChevronLeft, ChevronRight } from '@/components/icons'
+import { useQuery } from '@tanstack/react-query'
+import DefaultError from '@/components/layouts/default-error'
+
+export default function Page() {
+	const perPage = 1
+	const [currentPage, setCurrentPage] = useState<number>(1)
+	const [totalPages, setTotalPages] = useState<number>(1)
+	const [length, setLength] = useState<number>(1)
+
+	let pages = Array.from({ length: totalPages }, (_, index) => index + 1)
+	const maxVisiblePages = 9
+
+	if (totalPages > maxVisiblePages) {
+		const middlePage = Math.ceil(maxVisiblePages / 2)
+
+		if (currentPage <= middlePage) {
+			pages = [...pages.slice(0, maxVisiblePages - 2), -1, totalPages]
+		} else if (currentPage > totalPages - middlePage) {
+			pages = [1, -1, ...pages.slice(totalPages - maxVisiblePages + 2)]
+		} else {
+			const startPage = currentPage - Math.floor((maxVisiblePages - 3) / 2)
+			const endPage = currentPage + Math.floor((maxVisiblePages - 4) / 2)
+			pages = [1, -1, ...pages.slice(startPage, endPage), -1, totalPages]
+		}
+	}
+
+	const {
+		data: productsData,
+		isError,
+		isLoading,
+		isSuccess,
+		refetch
+	} = useQuery({
+		queryKey: [
+			'products',
+			{ sort: 'low-price' },
+			{
+				filters: {
+					min_price: undefined,
+					max_price: undefined,
+					searchTerm: undefined
+				}
+			},
+			{
+				pagination: {
+					page: currentPage,
+					limit: perPage
+				}
+			}
+		],
+		queryFn: () =>
+			ProductService.getAll({
+				sort: 'low-price',
+				min_price: undefined,
+				max_price: undefined,
+				searchTerm: undefined,
+				page: currentPage,
+				limit: perPage
+			}),
+		keepPreviousData: true
+	})
+
+	useEffect(() => {
+		if (isSuccess) {
+			setLength(productsData.length)
+			setTotalPages(Math.ceil(productsData.length / perPage))
+		}
+	}, [productsData, isSuccess])
+
+	if (isLoading) {
+		return <Spinner width="full" />
+	}
+
+	if (isError) {
+		return <DefaultError reset={refetch} />
+	}
+
+	const handlePrevPage = () => {
+		setCurrentPage(old => Math.max(old - 1, 1))
+	}
+
+	const handleNextPage = () => {
+		setCurrentPage(old => old + 1)
+	}
+
+	return (
+		<div className="flex flex-col gap-8">
+			<h3 className="font-bold text-3xl">Всі товари - {length}</h3>
+			<Products products={productsData.products} />
+			<div className="flex flex-col gap-8">
+				<div className="w-full items-center justify-center flex gap-8">
+					<Button shape="circle" onClick={handlePrevPage} disabled={currentPage === 1}>
+						<ChevronLeft />
+					</Button>
+					<div className="flex gap-2">
+						{pages.map((page, index) => (
+							<React.Fragment key={index}>
+								{page === -1 ? (
+									<span className="inline-flex items-center mx-2">...</span>
+								) : (
+									<Button
+										intent={`${currentPage === page ? 'primary' : 'secondary'}`}
+										onClick={() => {
+											if (page !== currentPage) {
+												setCurrentPage(page)
+											}
+										}}
+										disabled={currentPage === page}
+									>
+										{page}
+									</Button>
+								)}
+							</React.Fragment>
+						))}
+					</div>
+					<Button shape="circle" onClick={handleNextPage} disabled={currentPage >= totalPages}>
+						<ChevronRight />
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
+}
--- store/productStore.ts
+++ store/productStore.ts
@@ -1,3 +1,4 @@
+import { Product } from '@/types/product.type'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
